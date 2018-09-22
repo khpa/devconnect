75802315d1790d425b18d2d1ccd78c84d8f1b130
Christian Paun <christianpaun@gmail.com>
2018-09-22 18:29:34 +0100

    fixed errors

--- routes/api/users.js
+++ routes/api/users.js
@@ -22,21 +22,23 @@ router.get("/test", (req, res) =>
 //@access   Public
 
 router.post("/register", (req, res) => {
-  User.findOne({ email: req.body.email }).then(user => {
+  console.log(req.body);
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
     if (user) {
-      errors.email = "Email already exists";
-      return res.status(400).json(errors);
+      return res.status(400).json({ email: "Email already exists" });
     } else {
       const avatar = gravatar.url(req.body.email, {
-        s: "200", // Size
-        r: "pg", // Rating
-        d: "mm" // Default
+        s: 200,
+        r: "pg",
+        d: "mm"
       });
 
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
-        avatar,
+        avatar: avatar,
         password: req.body.password
       });
 
